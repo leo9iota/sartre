e16870044b5da468bd918b12aa960e9dd4efea50
leo9iota <leo9iota@gmail.com>
2024-12-12 20:08:36 +0100

    Add Hono HTTP exception

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,28 @@
 import { Hono } from 'hono';
+import { HTTPException } from 'hono/http-exception';
 
-const app = new Hono();
+import type { ErrorResponse } from '@/shared/types';
+
+const app = new Hono(); // Create new Hono instance
 
 app.get('/', (c) => {
     return c.text('Hello Hono!');
+});
+
+app.onError((err, c) => {
+    if (err instanceof HTTPException) {
+        const errResponse =
+            err.res ??
+            c.json<ErrorResponse>({
+                success: false,
+                error: err.message,
+                isFormError:
+                    err.cause && typeof err.cause === 'object' && 'form' in err.cause
+                        ? err.cause.form === true
+                        : false,
+            });
+        return errResponse;
+    }
 });
 
 export default app;

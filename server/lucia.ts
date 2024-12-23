6eaa7c402b80885ca9f2d3bc66f1e1f636fc41d0
leo9iota <leo9iota@gmail.com>
2024-12-23 15:56:42 +0100

    Add user attributes to Lucia auth

--- server/lucia.ts
+++ server/lucia.ts
@@ -1,15 +1,21 @@
-import { Lucia } from 'lucia'; // TODO: Revert to lucia@3.2.1 if errors occur with auth
+import { Lucia } from 'lucia'; // FIXME: Revert to lucia@3.2.1 if errors occur with auth
+
+import { adapter } from './adapter';
 
 export const lucia = new Lucia(adapter, {
     sessionCookie: {
         attributes: {
-            secure: process.env.NODE_ENV === 'production'
-        }
-    }
-})
+            secure: process.env.NODE_ENV === 'production',
+        },
+    },
+    getUserAttributes: (attr) => {
+        return { username: attr.username };
+    },
+});
 
 declare module 'lucia' {
     interface Register {
         Lucia: typeof lucia;
+        DatabaseUserAttributes: { username: string };
     }
 }

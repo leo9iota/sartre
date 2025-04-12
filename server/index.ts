107364f811e057efe3db1d05e72c96f7eea8d981
leo9iota <leo9iota@gmail.com>
2025-04-12 23:29:08 +0200

    Add authentication router and improve session handling

--- server/index.ts
+++ server/index.ts
@@ -5,13 +5,14 @@ import { HTTPException } from 'hono/http-exception';
 import { type ErrorResponse } from '@/shared/types';
 import type { Context } from './context';
 import { lucia } from './lucia';
+import { authRouter } from './routes/auth';
 
 const app = new Hono<Context>();
 
 app.get('/', (ctx) => {
     // Purposeful exception
     // throw new HTTPException(404, { message: 'Post not found', cause: { form: true } });
-    
+
     // Unexpected exception, when some logic in the code may break something that is unexpected
     // throw new Error("Unexpected error")
 
@@ -27,19 +28,27 @@ app.use('*', cors(), async (ctx, next) => {
         return next();
     }
 
+    // Check if session is active and is valid
     const { session, user } = await lucia.validateSession(sessionId);
+
     if (session && session.fresh) {
         ctx.header('Set-Cookie', lucia.createSessionCookie(session.id).serialize(), {
             append: true,
         });
     }
+
     if (!session) {
         ctx.header('Set-Cookie', lucia.createBlankSessionCookie().serialize(), { append: true });
     }
+
+    // If it is valid set it as context
     ctx.set('session', session);
     ctx.set('user', user);
     return next();
 });
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const routes = app.basePath('/api').route('/auth', authRouter);
 
 // Hono error handler
 app.onError((err, ctx) => {
@@ -74,3 +83,4 @@ app.onError((err, ctx) => {
 });
 
 export default app;
+export type ApiRoutes = typeof routes;

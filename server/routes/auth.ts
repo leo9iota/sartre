e523c655096cd171aa8986fe3001e881b6e85e6e
leo9iota <leo9iota@gmail.com>
2025-04-12 23:28:57 +0200

    Add login route to auth

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,8 +1,9 @@
 import { Hono } from 'hono';
 import { HTTPException } from 'hono/http-exception';
+import { eq } from 'drizzle-orm';
 
 import { db } from '@/adapter';
-import type { Context } from '@/context';
+import { type Context } from '@/context';
 import { userTable } from '@/db/schemas/auth';
 import { lucia } from '@/lucia';
 import { zValidator } from '@hono/zod-validator';
@@ -11,10 +12,8 @@ import postgres from 'postgres';
 
 import { loginSchema, type SuccessResponse } from '@/shared/types';
 
-export const authRouter = new Hono<Context>().post(
-    '/signup',
-    zValidator('form', loginSchema),
-    async (ctx) => {
+export const authRouter = new Hono<Context>()
+    .post('/signup', zValidator('form', loginSchema), async (ctx) => {
         const { username, password } = ctx.req.valid('form');
         const passwordHash = await Bun.password.hash(password);
         const userId = generateId(15);
@@ -40,9 +39,26 @@ export const authRouter = new Hono<Context>().post(
             );
         } catch (error) {
             if (error instanceof postgres.PostgresError && error.code === '23505') {
-                throw new HTTPException();
+                throw new HTTPException(409, { message: 'Username already exists' });
             }
             throw new HTTPException(500, { message: 'Failed to create user' });
         }
-    },
-);
+    })
+    .post('/login', zValidator('form', loginSchema), async (ctx) => {
+        const { username, password } = ctx.req.valid('form');
+
+        const [existingUser] = await db
+            .select()
+            .from(userTable)
+            .where(eq(userTable.username, username))
+            .limit(1);
+
+        if (!existingUser) {
+            throw new HTTPException(401, {
+                message: 'Incorrect username',
+            });
+        }
+
+        // Check if user entered valid password
+        const validPassword = await Bun.password.verify()
+    });

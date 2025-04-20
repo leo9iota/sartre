a8d333078f11ad47eefa8d747aa732954a353175
leo9iota <leo9iota@gmail.com>
2025-04-20 16:21:37 +0200

    Add database schema for posts

---
+++ server/db/schemas/posts.ts
@@ -0,0 +1,12 @@
+import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const postsTable = pgTable('posts', {
+    id: serial('id').primaryKey(),
+    userId: text('user_id').notNull(), // IMPORTANT: May want to set this to nullable for soft deleting, like Reddit, where the post of the deleted user account is still there
+    title: text('title').notNull(),
+    url: text('url'),
+    content: text('content'), // Either 'url' or 'content' cannot be null, we don't check this here
+    points: integer('points').default(0).notNull(),
+    commentCount: integer('comment_count').default(0).notNull(),
+    createdAt: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
+});

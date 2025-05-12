aab8f8da9985dfc96295f269cdf71a2b9014f102
leo9iota <leo9iota@gmail.com>
2025-05-12 22:12:08 +0200

    Add post schema with validation

--- shared/types.ts
+++ shared/types.ts
@@ -1,3 +1,4 @@
+import { insertPostSchema } from '@/db/schemas/posts';
 import { z } from 'zod';
 
 export type SuccessResponse<T = void> = {
@@ -19,3 +20,14 @@ export const loginSchema = z.object({
         .regex(/^[a-zA-Z0-9_]+$/),
     password: z.string().min(3).max(255),
 });
+
+export const createPostSchema = insertPostSchema
+    .pick({
+        title: true,
+        url: true,
+        content: true,
+    })
+    .refine((data) => data.url || data.content, {
+        message: 'Either the URL or content must be provided',
+        path: ['url', 'content'],
+    });

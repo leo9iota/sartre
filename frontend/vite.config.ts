15f10ebfeb5dd76c493f4dbe69d2b534d39d34d1
leo9iota <leo9iota@gmail.com>
2025-05-28 11:14:40 +0200

    Apply formatting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 

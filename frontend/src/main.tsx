2aa59c5d09435833a8a34ead3034a1f18a94c4a1
leo9iota <leo9iota@gmail.com>
2025-05-28 10:20:50 +0200

    Add integration for `QueryClientProvider` for routing setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,24 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import React from 'react';
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import ReactDOM from 'react-dom/client';
+
+import { routeTree } from './routeTree.gen';
+
+const queryClient = new QueryClient();
 
 // Set up a Router instance
 const router = createRouter({
   routeTree,
   defaultPreload: 'intent',
-})
+  defaultPreloadStaleTime: 0,
+  context: { queryClient },
+});
 
 // Register things for typesafety
 declare module '@tanstack/react-router' {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
-const rootElement = document.getElementById('app')!
+const rootElement = document.getElementById('app')!;
 
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(<RouterProvider router={router} />)
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>,
+  );
 }

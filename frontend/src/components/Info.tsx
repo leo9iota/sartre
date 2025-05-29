af6798153e5afda2972a49352925b4bac75b3000
leo9iota <leo9iota@gmail.com>
2025-05-29 17:38:11 +0200

    Add components

---
+++ frontend/src/components/Info.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function Info({}: Props) {
+  return (
+    <div>Info</div>
+  )
+}
+
+export default Info
---
+++ frontend/src/components/Sort.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function Sort({}: Props) {
+  return (
+    <div>Sort</div>
+  )
+}
+
+export default Sort

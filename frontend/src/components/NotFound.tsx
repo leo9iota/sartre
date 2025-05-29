619caf5edb64014a93101c40c7daf74b9ee65975
leo9iota <leo9iota@gmail.com>
2025-05-29 18:12:33 +0200

    Refactor `NotFound` component to display  404 error message and navigation link

--- frontend/src/components/NotFound.tsx
+++ frontend/src/components/NotFound.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import { Link } from '@tanstack/react-router';
 
-type Props = {}
+import { Button } from './ui/button';
 
-function NotFound({}: Props) {
+export function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <div className='flex size-full items-center justify-center p-2 text-2xl'>
+      <div className='flex flex-col items-center gap-4'>
+        <p className='text-4xl font-bold'>404</p>
+        <p className='text-lg'>Page not found</p>
+        <Button asChild>
+          <Link to='/'>Go Home</Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
-
-export default NotFound

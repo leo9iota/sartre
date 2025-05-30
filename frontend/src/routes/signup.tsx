eea100a90eae320db0adb2ec601619c7a787cfd9
leo9iota <leo9iota@gmail.com>
2025-05-30 22:58:29 +0200

    Remove unused code

--- frontend/src/routes/signup.tsx
+++ frontend/src/routes/signup.tsx
@@ -1,31 +1,12 @@
-import {
-  createFileRoute,
-  Link,
-  redirect,
-  useNavigate,
-  useRouter,
-} from '@tanstack/react-router';
+import { createFileRoute } from '@tanstack/react-router';
 import { useForm } from '@tanstack/react-form';
-import { useQueryClient } from '@tanstack/react-query';
 import { fallback, zodSearchValidator } from '@tanstack/router-zod-adapter';
 
-
-import { toast } from 'sonner';
 import { z } from 'zod';
 
 import { loginSchema } from '@/shared/types';
 // import { postSignup, userQueryOptions } from '@/lib/api';
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import Info from '@/components/Info';
+import { Card } from '@/components/ui/card';
 
 const signupSearchSchema = z.object({
   redirect: fallback(z.string(), '/').default('/'),
@@ -43,11 +24,6 @@ export const Route = createFileRoute('/signup')({
 });
 
 function Signup() {
-  const search = Route.useSearch();
-  const navigate = useNavigate();
-  const router = useRouter();
-  const queryClient = useQueryClient();
-
   const form = useForm({
     defaultValues: {
       username: '',
@@ -55,22 +31,6 @@ function Signup() {
     },
     validators: {
       onChange: loginSchema,
-    },
-    onSubmit: async ({ value }) => {
-      const res = await postSignup(value.username, value.password);
-      if (res.success) {
-        await queryClient.invalidateQueries({ queryKey: ['user'] });
-        router.invalidate();
-        await navigate({ to: search.redirect });
-        return null;
-      } else {
-        if (!res.isFormError) {
-          toast.error('Signup failed', { description: res.error });
-        }
-        form.setErrorMap({
-          onSubmit: res.isFormError ? res.error : 'Unexpected error',
-        });
-      }
     },
   });
   return (
@@ -82,75 +42,7 @@ function Signup() {
             event.stopPropagation();
             form.handleSubmit();
           }}
-        >
-          <CardHeader>
-            <CardTitle className='text-center text-2xl'>Signup</CardTitle>
-            <CardDescription>
-              Enter your details below to create an account
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className='grid gap-4'>
-              <form.Field
-                name='username'
-                children={(field) => (
-                  <div className='grid gap-2'>
-                    <Label htmlFor={field.name}>Username</Label>
-                    <Input
-                      id={field.name}
-                      name={field.name}
-                      value={field.state.value}
-                      onBlur={field.handleBlur}
-                      onChange={(event) => field.handleChange(event.target.value)}
-                    />
-                    <Info field={field} />
-                  </div>
-                )}
-              />
-              <form.Field
-                name='password'
-                children={(field) => (
-                  <div className='grid gap-2'>
-                    <Label htmlFor={field.name}>Password</Label>
-                    <Input
-                      type='password'
-                      id={field.name}
-                      name={field.name}
-                      value={field.state.value}
-                      onBlur={field.handleBlur}
-                      onChange={(event) => field.handleChange(event.target.value)}
-                    />
-                    <Info field={field} />
-                  </div>
-                )}
-              />
-              <form.Subscribe
-                selector={(state) => [state.errorMap]}
-                children={([errorMap]) =>
-                  errorMap.onSubmit ? (
-                    <p className='text-[0.8rem] font-medium text-destructive'>
-                      {errorMap.onSubmit?.toString()}
-                    </p>
-                  ) : null
-                }
-              />
-              <form.Subscribe
-                selector={(state) => [state.canSubmit, state.isSubmitting]}
-                children={([canSubmit, isSubmitting]) => (
-                  <Button type='submit' disabled={!canSubmit} className='w-full'>
-                    {isSubmitting ? '...' : 'Signup'}
-                  </Button>
-                )}
-              />
-            </div>
-            <div className='mt-4 text-center text-sm'>
-              Alrerady have an account?{' '}
-              <Link to='/login' className='underline'>
-                Log in
-              </Link>
-            </div>
-          </CardContent>
-        </form>
+        ></form>
       </Card>
     </div>
   );

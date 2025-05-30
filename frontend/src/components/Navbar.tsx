0fe759a3a6a944eb0fda2b2c724b4f6866c4cc9e
leo9iota <leo9iota@gmail.com>
2025-05-30 16:54:32 +0200

    Refactor `Navbar` component by removing unused imports and simplifying the structure

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,17 +1,5 @@
 import { Link } from '@tanstack/react-router';
 
-import { MenuIcon } from 'lucide-react';
-
-import { Button } from './ui/button';
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from './ui/sheet';
-
 function Navbar() {
   return (
     <header className='sticky top-0 z-50 w-full border-border/40 bg-primary/95 backdrop-blur supports-[backdrop-filter]:bg-primary/90'>
@@ -35,100 +23,14 @@ function Navbar() {
             >
               top
             </Link>
-            <Link to='/submit' className='hover:underline'>
+            <Link to='/' className='hover:underline'>
               submit
             </Link>
           </nav>
         </div>
-        <div className='hidden items-center space-x-4 md:flex'>
-          {user ? (
-            <>
-              <span>{user}</span>
-              <Button
-                asChild
-                size='sm'
-                variant='secondary'
-                className='bg-secondary-foreground text-primary-foreground hover:bg-secondary-foreground/70'
-              >
-                <a href='api/auth/logout'>Log out</a>
-              </Button>
-            </>
-          ) : (
-            <Button
-              asChild
-              size='sm'
-              variant='secondary'
-              className='bg-secondary-foreground text-primary-foreground hover:bg-secondary-foreground/70'
-            >
-              <Link to='/login'>Log in</Link>
-            </Button>
-          )}
-        </div>
-        <Sheet open={isOpen} onOpenChange={setIsOpen}>
-          <SheetTrigger asChild>
-            <Button variant='secondary' size='icon' className='md:hidden'>
-              <MenuIcon className='size-6' />
-            </Button>
-          </SheetTrigger>
-          <SheetContent className='mb-2'>
-            <SheetHeader>
-              <SheetTitle>BetterNews</SheetTitle>
-              <SheetDescription className='sr-only'>Navigation</SheetDescription>
-            </SheetHeader>
-            <nav className='flex flex-col space-y-4'>
-              <Link
-                onClick={() => setIsOpen(false)}
-                className='hover:underline'
-                to={'/'}
-                search={{ sortBy: 'recent', order: 'desc' }}
-              >
-                new
-              </Link>
-              <Link
-                onClick={() => setIsOpen(false)}
-                className='hover:underline'
-                to={'/'}
-                search={{ sortBy: 'points', order: 'desc' }}
-              >
-                top
-              </Link>
-              <Link
-                onClick={() => setIsOpen(false)}
-                className='hover:underline'
-                to='/submit'
-              >
-                submit
-              </Link>
-              {user ? (
-                <>
-                  <span>user: {user}</span>
-                  <Button
-                    asChild
-                    size='sm'
-                    variant='secondary'
-                    className='bg-secondary-foreground text-primary-foreground hover:bg-secondary-foreground/70'
-                  >
-                    <a href='api/auth/logout'>Log out</a>
-                  </Button>
-                </>
-              ) : (
-                <Button
-                  asChild
-                  size='sm'
-                  variant='secondary'
-                  className='bg-secondary-foreground text-primary-foreground hover:bg-secondary-foreground/70'
-                >
-                  <Link onClick={() => setIsOpen(false)} to='/login'>
-                    Log in
-                  </Link>
-                </Button>
-              )}
-            </nav>
-          </SheetContent>
-        </Sheet>
       </div>
     </header>
   );
-};
+}
 
 export default Navbar;

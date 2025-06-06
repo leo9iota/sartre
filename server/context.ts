750b7291d6377759adf9c00f3215337593a36787
leo9iota <leo9iota@gmail.com>
2025-06-06 13:15:58 +0200

    Define `Session` and `User` interfaces in for improved type safety

--- server/context.ts
+++ server/context.ts
@@ -1,6 +1,18 @@
 import type { Env } from 'hono';
 
-import type { Session, User } from 'lucia';
+export interface Session {
+    id: string;
+    token: string;
+    expiresAt: Date;
+    userId: string;
+}
+
+export interface User {
+    id: string;
+    username: string;
+    email: string | null;
+    name: string | null;
+}
 
 export interface Context extends Env {
     Variables: {

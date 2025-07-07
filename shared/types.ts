d3fce780f61c7e108c5d66b45133ce4411ee52c2
leo9iota <leo9iota@gmail.com>
2025-07-07 16:26:23 +0200

    Refactor structure

--- shared/types.ts
+++ shared/types.ts
@@ -15,6 +15,49 @@ export type ErrorResponse = {
     isFormError?: boolean;
 };
 
+export type Post = {
+    id: number;
+    title: string;
+    url: string | null;
+    content: string | null;
+    points: number;
+    createdAt: string;
+    commentCount: number;
+    author: {
+        id: string;
+        username: string;
+    };
+    isUpvoted: boolean;
+};
+
+export type Comment = {
+    id: number;
+    userId: string;
+    content: string;
+    points: number;
+    depth: number;
+    commentCount: number;
+    createdAt: string;
+    postId: number;
+    parentCommentId: number | null;
+    commentUpvotes: {
+        userId: string;
+    }[];
+    author: {
+        username: string;
+        id: string;
+    };
+    childComments?: Comment[];
+};
+
+export type PaginatedResponse<T> = {
+    pagination: {
+        page: number;
+        totalPages: number;
+    };
+    data: T;
+} & Omit<SuccessResponse, 'data'>;
+
 export const loginSchema = z.object({
     username: z
         .string()
@@ -72,46 +115,3 @@ export const createCommentSchema = z.object({
         .string()
         .min(3, { message: 'Comment must be at least 3 characters long' }),
 });
-
-export type Post = {
-    id: number;
-    title: string;
-    url: string | null;
-    content: string | null;
-    points: number;
-    createdAt: string;
-    commentCount: number;
-    author: {
-        id: string;
-        username: string;
-    };
-    isUpvoted: boolean;
-};
-
-export type Comment = {
-    id: number;
-    userId: string;
-    content: string;
-    points: number;
-    depth: number;
-    commentCount: number;
-    createdAt: string;
-    postId: number;
-    parentCommentId: number | null;
-    commentUpvotes: {
-        userId: string;
-    }[];
-    author: {
-        username: string;
-        id: string;
-    };
-    childComments?: Comment[];
-};
-
-export type PaginatedResponse<T> = {
-    pagination: {
-        page: number;
-        totalPages: number;
-    };
-    data: T;
-} & Omit<SuccessResponse, 'data'>;

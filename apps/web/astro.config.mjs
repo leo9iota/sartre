4e2132bf6009552c0ddcca0b9f45c2c55b520b92
leo9iota <leo9iota@gmail.com>
2026-01-20 12:13:46 +0100

    perf(astro): optimize vite dependency pre-bundling for vanilla-extract

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,13 +1,13 @@
-// @ts-check
 import solidJs from '@astrojs/solid-js';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-// Forces restart
 export default defineConfig({
     integrations: [solidJs()],
     vite: {
-        plugins: [vanillaExtractPlugin()]
+        plugins: [vanillaExtractPlugin()],
+        optimizeDeps: {
+            include: ['@vanilla-extract/css', '@vanilla-extract/recipes']
+        }
     }
 });
